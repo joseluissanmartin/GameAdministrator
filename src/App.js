9487360b93d04e9436af99eaa6a1350ee018f121
joseluissanmartin <64567304+joseluissanmartin@users.noreply.github.com>
2020-06-15 15:30:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,13 @@ import './App.css';
 import Article from './Components/article';
 import Header from './Components/header';
 import Section from './Components/section';
-import Footer from './Components/footer';
-import Head from './Components/head';
 
 function App(){
     return(
       <div className="App">
-      <Head />
       <Header />
       <Section />
       <Article />
-      <Footer />
       </div>
     );
   }
