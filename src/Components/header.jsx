8da0d7cc05f721021b04181c2472ce1399567984
jose luis san martin <joseluis_sanmartin@icloud.com>
2020-06-15 15:15:05 -0400

    Se realizaron cambios de funcionalidad

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
-
+import Titulo from './Images/titulo.png';
+import Logo from './Images/logo.png';
 
 export default function Header(){
   return(
     <header>
-      <div class = "tarjeta"
-        />
-        <h1>Game Administrator</h1>
+      <div class = "tarjeta"/>
+      <img class ="imagen0" src={Logo} alt="A Logo"/>
+      <img class="imagenTitulo" src= {Titulo} alt="A Titulo" />
 
       <div class= "registro">
      <form>
       <div class="email">
         <input type="email" name="email" placeholder=" Usuario o e-mail" />
       </div>
-      <div>
+      <div class="password">
         <input type="password" name="password" placeholder=" Contrase&ntilde;a" />
       </div>
       <p>
-        <div class= "u">
+        <div class="u">
     <input type="radio" name="ua" value="u" required /><h4>Usuario</h4>
         </div>
-        <div class= "a">
+        <div class="a">
     <input type="radio" name="ua" value="a" required /><h5>Administrador</h5>
         </div>
      </p>
--- src/Components/section.jsx
+++ src/Components/section.jsx
@@ -5,7 +5,7 @@ export default function Section(){
   return(
     <section>
       <ul>
-       <li><a href="registro.html">Registrar</a></li>
+       <li><a href="registro.html">Registrarse</a></li>
        <li><a href="catalogo.html">Catalogo</a></li>
       </ul>
     </section>
