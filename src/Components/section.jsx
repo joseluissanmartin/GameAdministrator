438b8dd594fbc8d2a359c4a39140d87db9622043
joseluissanmartin <64567304+joseluissanmartin@users.noreply.github.com>
2020-06-17 13:44:12 -0400

    Update section.jsx

--- src/Components/section.jsx
+++ src/Components/section.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-
+import Registro from '../Pages/registro';
+import Catalogo from '../Pages/catalogo';
 
 export default function Section(){
   return(
     <section>
-      <ul>
-       <li><a href="registro.html">Registrar</a></li>
-       <li><a href="catalogo.html">Catalogo</a></li>
-      </ul>
+
+    <ul>
+       <li><a href=<Registro />>Registrarse</a></li>
+       <li><a href=<Catalogo />> ir a catalogo</a></li>
+    </ul>
+
     </section>
   );
 }
